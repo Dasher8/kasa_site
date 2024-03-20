61367ef9f03f092ce564f984c57056a2d26028ce
Dasher <dasha.topor@gmail.com>
2024-03-20 18:13:42 +0100

    feat: create error page

--- src/pages/error404/index.jsx
+++ src/pages/error404/index.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+import "./styles.scss";
+
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+
 export default function Error404() {
-  return <div>error 404</div>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <a href="#">
+          <p className="return-link">Retourner sur la page d’accueil</p>
+        </a>
+      </div>
+      <Footer />
+    </>
+  );
 }
