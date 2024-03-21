f390a54cbf1ddcb76a0b43a31fe1b25cd226ff7a
Dasher <dasha.topor@gmail.com>
2024-03-21 10:52:41 +0100

    feat: create dropdown component for the equipment

--- src/components/dropdown/index.jsx
+++ src/components/dropdown/index.jsx
@@ -1,28 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles.scss";
 
 function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <li class="dropdown">
-        <a href="javascript:void(0)">
-          <div className="drop-btn">
-            <p>Équipements</p>
-            <i class="fa-solid fa-chevron-up"></i>
-            <i class="fa-solid fa-chevron-down"></i>
-          </div>
-        </a>
-        <div class="dropdown-content">
-          <p className="first-tag">Climatisation</p>
-          <p>Wi-Fi</p>
-          <p> Cuisine</p>
-          <p>Espace de travail</p>
-          <p>Fer à repasser</p>
-          <p>Sèche-cheveux</p>
-          <p className="last-tag">Cintres</p>
+      <div className="dropdown">
+        <div className="drop-btn" onClick={toggleDropdown}>
+          <p>Équipements</p>
+          <i className={`fa-solid fa-chevron-${isOpen ? "down" : "up"}`}></i>
         </div>
-      </li>
+        {isOpen && (
+          <ul className="dropdown-content">
+            <li className="first-tag">Climatisation</li>
+            <li>Wi-Fi</li>
+            <li>Cuisine</li>
+            <li>Espace de travail</li>
+            <li>Fer à repasser</li>
+            <li>Sèche-cheveux</li>
+            <li className="last-tag">Cintres</li>
+          </ul>
+        )}
+      </div>
     </>
   );
 }
