942d97c1b42a5b59c674b59ca32a3ce875c6f903
Dasher <dasha.topor@gmail.com>
2024-04-07 14:32:55 +0200

    feat: create slide indicator with numbers

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,25 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import "./styles.scss"
+import "./styles.scss";
 
- const Carousel = ({ data }) => {
-    const [slide, setSlide] = useState(0);
+const Carousel = ({ data }) => {
+  const [slide, setSlide] = useState(0);
 
-    const nextSlide = () => {
-        setSlide(slide === data.length - 1 ? 0 : slide + 1);
-    };
+  const nextSlide = () => {
+    setSlide(slide === data.length - 1 ? 0 : slide + 1);
+  };
 
-    const prevSlide = () => {
-        setSlide(slide === 0 ? data.length - 1 : slide - 1);
-    };
+  const prevSlide = () => {
+    setSlide(slide === 0 ? data.length - 1 : slide - 1);
+  };
 
-    return <div className="carousel">
-        <i class="fa-solid fa-chevron-left" onClick={prevSlide}></i>
-        {data.map((item, index) => {
-            return <img src={item.src} alt={item.alt} key={index} className={slide === index ? "slide" : "slide slide-hidden"}/>
-        })}
-        <i class="fa-solid fa-chevron-right" onClick={nextSlide}></i>
+  return (
+    <div className="carousel">
+      <i class="fa-solid fa-chevron-left" onClick={prevSlide}></i>
+      {data.map((item, index) => {
+        return (
+          <img
+            src={item.src}
+            alt={item.alt}
+            key={index}
+            className={slide === index ? "slide" : "slide slide-hidden"}
+          />
+        );
+      })}
+      <i class="fa-solid fa-chevron-right" onClick={nextSlide}></i>
+      <div className="slide-indicator">{`${slide + 1}/${data.length}`}</div>
     </div>
-}
+  );
+};
 
 export default Carousel;
