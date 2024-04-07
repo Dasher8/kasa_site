2e49b149fa6315d14e2b2557f038d3248caf860a
Dasher <dasha.topor@gmail.com>
2024-04-07 13:02:07 +0200

    feat: make carousel functional

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,14 +1,24 @@
-import React  from "react";
+import React, {useState} from "react";
 
 import "./styles.scss"
 
  const Carousel = ({ data }) => {
+    const [slide, setSlide] = useState(0);
+
+    const nextSlide = () => {
+        setSlide(slide === data.length - 1 ? 0 : slide + 1);
+    };
+
+    const prevSlide = () => {
+        setSlide(slide === 0 ? data.length - 1 : slide - 1);
+    };
+
     return <div className="carousel">
-        <i class="fa-solid fa-chevron-left"></i>
+        <i class="fa-solid fa-chevron-left" onClick={prevSlide}></i>
         {data.map((item, index) => {
-            return <img src={item.src} alt={item.alt} key={index} className="slide"/>
+            return <img src={item.src} alt={item.alt} key={index} className={slide === index ? "slide" : "slide slide-hidden"}/>
         })}
-        <i class="fa-solid fa-chevron-right"></i>
+        <i class="fa-solid fa-chevron-right" onClick={nextSlide}></i>
     </div>
 }
 
