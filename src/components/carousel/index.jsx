8737a818c433cddfd2a88ab1e823696710824d0a
Dasher <dasha.topor@gmail.com>
2024-04-07 12:48:53 +0200

    feat: style the carousel

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React  from "react";
+
+import "./styles.scss"
 
  const Carousel = ({ data }) => {
-    return <div>
+    return <div className="carousel">
+        <i class="fa-solid fa-chevron-left"></i>
         {data.map((item, index) => {
-            return <img src={item.src} alt={item.alt} key={index} />
+            return <img src={item.src} alt={item.alt} key={index} className="slide"/>
         })}
+        <i class="fa-solid fa-chevron-right"></i>
     </div>
 }
 
