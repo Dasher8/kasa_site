d5b1b691a4097bd1a536114c2b608c90487304d1
Dasher <dasha.topor@gmail.com>
2024-04-10 18:38:44 +0200

    fix: fix blanck splace after footer on the flat/error page

--- src/pages/flat/index.jsx
+++ src/pages/flat/index.jsx
@@ -34,18 +34,18 @@ export default function Flat() {
     <div className="flat">
       <Header />
       <div className="flat-main">
-      {flat && <Carousel pictures={flat.pictures} title={flat.title} />}
-      <Description
-        title={flat.title}
-        location={flat.location}
-        tags={flat.tags}
-        description={flat.description}
-        equipments={flat.equipments}
-        host={flat.host}
-        rating={flat.rating}
-      />
-      <Footer />
+        {flat && <Carousel pictures={flat.pictures} title={flat.title} />}
+        <Description
+          title={flat.title}
+          location={flat.location}
+          tags={flat.tags}
+          description={flat.description}
+          equipments={flat.equipments}
+          host={flat.host}
+          rating={flat.rating}
+        />
       </div>
+      <Footer />
     </div>
   );
 }
