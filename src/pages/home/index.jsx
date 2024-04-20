71d4f1e6127fe7bb2bd5234e39f7b8b4907267f0
Dasher <dasha.topor@gmail.com>
2024-04-20 15:39:39 +0200

    fix: fix blackspace after footer
    Co-authored-by: William Malbos <contact@wmalbos.fr>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import LayoutDefault from "../../layouts/default";
 import Cards from "../../containers/cards";
 import Banner from "../../components/banner";
@@ -7,25 +7,10 @@ import imageBanner from "../../assets/banner.svg";
 import "./styles.scss";
 
 export default function Home() {
-  const [flat, setFlat] = useState(null);
-
-  useEffect(() => {
-    fetch("/data/flats.json")
-      .then((response) => response.json())
-      .then((data) => {
-        if (data && Array.isArray(data.data) && data.data.length > 0) {
-          setFlat(data.data[0]); // Set the first flat from the data array
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching flats:", error);
-      });
-  }, []);
-
   return (
     <LayoutDefault page="home">
       <Banner src={imageBanner} title={"Chez vous, partout et ailleurs"} />
-      <Cards flat={flat} />
+      <Cards />
     </LayoutDefault>
   );
 }
